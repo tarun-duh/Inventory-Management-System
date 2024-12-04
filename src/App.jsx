d7df4a6a11ca709a54d6d1a107c1342a003d30d5
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-12-04 13:46:42 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl text-center font-bold underline">
-        INVENTORY MANAGEMENT SISHTUMMM
+        INVENTORY MANAGEMENT SISHTUMMM!!!!!!!!!!!!!!
       </h1>
     </>
   );
